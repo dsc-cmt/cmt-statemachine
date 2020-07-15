1553b0a8c63628fb9cdce9238f669856238debfd
dingchenchen <1936886538@qq.com>
2020-07-15 16:11:38 +0800

    feature:生成 plantuml.txt 文件，用于生成状态图

--- src/main/java/com/cmt/statemachine/impl/PlantUMLVisitor.java
+++ src/main/java/com/cmt/statemachine/impl/PlantUMLVisitor.java
@@ -5,6 +5,13 @@ import com.cmt.statemachine.StateMachine;
 import com.cmt.statemachine.Transition;
 import com.cmt.statemachine.Visitor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * PlantUMLVisitor
  *
@@ -12,6 +19,11 @@ import com.cmt.statemachine.Visitor;
  * @date 2020-02-09 7:47 PM
  */
 public class PlantUMLVisitor implements Visitor {
+
+    /**
+     * 存储构建PlantUML状态图的语句
+     */
+    private List<String> plantUMLStatements;
 
     /**
      * Since the state machine is stateless, there is no initial state.
@@ -23,18 +35,36 @@ public class PlantUMLVisitor implements Visitor {
      */
     @Override
     public void visitOnEntry(StateMachine<?, ?> visitable) {
+        plantUMLStatements = new ArrayList<>();
         System.out.println("@startuml");
+        plantUMLStatements.add("@startuml");
     }
 
     @Override
     public void visitOnExit(StateMachine<?, ?> visitable) {
         System.out.println("@enduml");
+        plantUMLStatements.add("@enduml");
+        //生成 plantuml.txt 文件
+        plantUMLStatements.forEach(statement->{
+            File file = new File("plantuml.txt");
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(file, true);
+                PrintStream out = new PrintStream(fos);
+                String str = statement +"\r\n";
+                out.print(str);
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void visitOnEntry(State<?, ?> state) {
         for(Transition transition: state.getTransitions()){
             System.out.println(transition.getSource().getId()+" --> "+transition.getTarget().getId()+" : "+transition.getEvent());
+            plantUMLStatements.add(transition.getSource().getId()+" --> "+transition.getTarget().getId()+" : "+transition.getEvent());
         }
     }
 
